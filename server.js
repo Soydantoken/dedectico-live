7d6f4f28217d7b6382b8a63aed031156681c5e5c
Enginfatma <enginfatmasoydan30@gmail.com>
2025-06-23 04:47:43 +0300

    Dedectico canlı sistem yüklendi

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname)); // index.html erişimi
+
+app.post('/log', (req, res) => {
+  const logData = req.body;
+  const logLine = JSON.stringify(logData) + '\n';
+  fs.appendFileSync('dedectico_logs.txt', logLine);
+  res.status(200).send('Veri kaydedildi.');
+});
+
+app.listen(port, () => {
+  console.log(`Dedectico GPS + IP sistemi ${port} portunda çalışıyor.`);
+});
